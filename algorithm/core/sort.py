46e62e201a85a1bfe1825d37220aa2452a2a1041
Yaohua <chinatsui1987@gmail.com>
2019-04-26 20:49:31 +0800

    Sorting algorithm review #190426
    1. Bubble sort, insertion sort, selection sort
    2. Quick sort, merge sort, heap sort
    3. Bucket sort, counting sort

--- algorithm/core/sort.py
+++ algorithm/core/sort.py
@@ -2,7 +2,30 @@ def swap(nums, src, dst):
     nums[src], nums[dst] = nums[dst], nums[src]
 
 
+def merge(nums, lo, mi, hi):
+    aux = nums[:]
+
+    left, right = lo, mi + 1
+    for i in range(lo, hi + 1):
+        if left > mi:
+            nums[i] = aux[right]
+            right += 1
+        elif right > hi:
+            nums[i] = aux[left]
+            left += 1
+        elif aux[left] <= aux[right]:
+            nums[i] = aux[left]
+            left += 1
+        else:
+            nums[i] = aux[right]
+            right += 1
+
+
 class QuickSort:
+    """
+    O(N*logN), in-place, unstable sorting algorithm
+    """
+
     def sort(self, nums):
         if not nums:
             return
@@ -27,6 +50,10 @@ class QuickSort:
 
 
 class QuickSort2:
+    """
+    O(N*logN), in-place, unstable sorting algorithm
+    """
+
     @staticmethod
     def sort(nums):
         if not nums:
@@ -55,81 +82,54 @@ class QuickSort2:
 
 
 class MergeSort:
+    """
+    O(N*logN), non in-place, stable sorting algorithm
+    """
+
     def sort(self, nums):
         if not nums:
             return
 
-        left = 0
-        right = len(nums) - 1
-        self._sort(nums, left, right)
+        self._sort(nums, 0, len(nums) - 1)
 
-    def _sort(self, nums, left, right):
-        if left >= right:
+    def _sort(self, nums, lo, hi):
+        if lo >= hi:
             return
 
-        mid = (left + right) // 2
-        self._sort(nums, left, mid)
-        self._sort(nums, mid + 1, right)
-        self._merge(nums, left, mid, right)
-
-    @staticmethod
-    def _merge(nums, left, mid, right):
-        aux = nums[:]
-        m, n = left, mid + 1
-        for i in range(left, right + 1):
-            if m > mid:  # left is over
-                nums[i] = aux[n]
-                n += 1
-            elif n > right:  # right is over
-                nums[i] = aux[m]
-                m += 1
-            elif aux[m] < aux[n]:
-                nums[i] = aux[m]
-                m += 1
-            else:
-                nums[i] = aux[n]
-                n += 1
+        mi = (lo + hi) // 2
+        self._sort(nums, lo, mi)
+        self._sort(nums, mi + 1, hi)
+        merge(nums, lo, mi, hi)
 
 
 class MergeSort2:
+    """
+    O(N*logN), non in-place, stable sorting algorithm
+    """
+
     def sort(self, nums):
         if not nums:
             return
 
-        size = len(nums)
+        n = len(nums)
         i = 1
-        while i < size:
-            left = 0
-            while left + i - 1 < size:
-                mid = left + i - 1
-                right = mid + i
-                if right >= size:
-                    right = size - 1
-                self._merge(nums, left, mid, right)
-                left += 2 * i
+        while i < n:
+            lo = 0
+            while lo + i - 1 < n:
+                mi = lo + i - 1
+                hi = mi + i
+                if hi >= n:
+                    hi = n - 1
+                merge(nums, lo, mi, hi)
+                lo = hi + 1
             i *= 2
-
-    @staticmethod
-    def _merge(nums, left, mid, right):
-        aux = nums[:]
-
-        m, n = left, mid + 1
-        for i in range(left, right + 1):
-            if m > mid:
-                nums[i] = aux[n]
-                n += 1
-            elif n > right:
-                nums[i] = aux[m]
-                m += 1
-            elif aux[m] < aux[n]:
-                nums[i] = aux[m]
-                m += 1
-            else:
-                nums[i] = aux[n]
-                n += 1
 
 
 class HeapSort:
+    """
+    O(N*logN), in-place, unstable sorting algorithm
+    """
+
     def sort(self, nums):
         if not nums:
             return
@@ -165,19 +165,30 @@ class HeapSort:
 
 
 class BubbleSort:
+    """
+    O(N^2), in-place, stable soring algorithm
+    """
+
     @staticmethod
     def sort(nums):
         if not nums:
             return
 
         n = len(nums)
-        for i in range(n - 1):
+        for i in range(n):
+            is_sorted = True
             for j in range(n - i - 1):
                 if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    is_sorted = False
+                    swap(nums, j, j + 1)
+            if is_sorted:
+                break
 
 
 class InsertionSort:
+    """
+    O(N^2), in-place, stable sorting algorithm
+    """
 
     @staticmethod
     def sort(nums):
@@ -188,13 +199,87 @@ class InsertionSort:
         for i in range(1, n):
             for j in range(i - n, -n, -1):
                 if nums[j] < nums[j - 1]:
-                    nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                    swap(nums, j, j - 1)
+                else:
+                    break
 
 
-t_nums = [25, 68, 68, 62, 68, 56, 30, 56,
-          32, 64, 41, 6, 39, 19, 28, 46,
-          11, 18, 53, 59, 11, 71, 73, 75,
-          76, 80, 83, 85, 91, 99]
+class SelectionSort:
+    """
+    O(N^2), in-place, unstable sorting algorithm
+    """
+
+    @staticmethod
+    def sort(nums):
+        if not nums:
+            return
+
+        n = len(nums)
+        for i in range(n):
+            _min = i
+            for j in range(i + 1, n):
+                if nums[j] < nums[_min]:
+                    _min = j
+            swap(nums, i, _min)
+
+
+class BucketSort:
+    """
+    O(N), not in-place, stable sorting algorithm
+    """
+
+    @staticmethod
+    def sort(nums):
+        """
+        For convenience, assume all numbers in array are non negative numbers.
+        """
+        if not nums:
+            return
+
+        _max = max(nums)
+        bucket = [[] for _ in range(_max + 1)]
+        for num in nums:
+            bucket[num].append(num)
+
+        i = 0
+        for buc in bucket:
+            if buc:
+                for num in buc:
+                    nums[i] = num
+                    i += 1
+
+
+class CountingSort:
+    """
+    O(N), not in-place, stable sorting algorithm
+    """
+
+    @staticmethod
+    def sort(nums):
+        """
+        For convenience, assume all numbers in array are non negative numbers.
+        """
+        if not nums:
+            return
+
+        _max = max(nums)
+        cnt_arr = [0 for _ in range(_max + 1)]
+
+        for num in nums:
+            cnt_arr[num] += 1
+
+        for i in range(1, _max + 1):
+            cnt_arr[i] += cnt_arr[i - 1]
+
+        aux = nums[:]
+        for i in range(-1, -len(aux) - 1, -1):
+            num = aux[i]
+            cnt = cnt_arr[num]
+            nums[cnt - 1] = num
+            cnt_arr[num] -= 1
+
+
+t_nums = [25, 68, 62, 68, 56, 30, 56, 32, 64, 41, 6, 39, 19, 28, 46, 11, 18, 53, 11, 71, 73, 75, 76, 80, 83, 85, 91, 99]
 # QuickSort().sort(t_nums)
 # MergeSort().sort(t_nums)
 # QuickSort2().sort(t_nums)
@@ -202,4 +287,7 @@ t_nums = [25, 68, 68, 62, 68, 56, 30, 56,
 # HeapSort().sort(t_nums)
 # BubbleSort().sort(t_nums)
 # InsertionSort().sort(t_nums)
-# print(t_nums)
+# SelectionSort().sort(t_nums)
+# BucketSort().sort(t_nums)
+CountingSort().sort(t_nums)
+print(t_nums)
